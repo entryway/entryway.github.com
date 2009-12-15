23be97f4728fd63b4d4e28253a6c6c3657d49273
Katie Roberts <katie@ladia-2.local>
2009-12-15 09:52:41 -0500

    added active navigation javascript and home page image

--- javascripts/application.js
+++ javascripts/application.js
@@ -2,7 +2,11 @@
 $(function () {
 
   $('div.expandable p').expander();
-
+  
+  var path = window.location.href.split("/").slice(-1)[0];
+  var selector = 'a[href=\'/' + path + '\']';
+  $(selector).addClass('active');
+  
 });
 
 
