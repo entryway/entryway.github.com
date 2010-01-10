6cfc5cecef4189f755dc61d8287feb5b9e36702f
gustin <gustin@entryway.net>
2010-01-10 01:50:28 -0500

    tweaked twitter feeds with promopts being specific to users "gustin proclaimed", altered the process page copy a bit

--- javascripts/application.js
+++ javascripts/application.js
@@ -25,24 +25,35 @@ $(function () {
   $(selector).addClass('active');
 
   $('div#entryway-tweet').tweet({
-    username: 'entryway',
-    count: 1
+username: 'entryway',
+    count: 1,
+    join_text: 'auto',
+    auto_join_text_default: "we proclaimed,", 
+    auto_join_text_reply: 'we replied to'
   });
 
   $('div#gustin-tweet').tweet({
     username: 'gustin',
+    count: 1,
     join_text: 'auto',
-    count: 1
+    auto_join_text_reply: 'gustin replied to',
+    auto_join_text_default: 'gustin proclaimed,' 
   });
 
   $('div#katie-tweet').tweet({
     username: 'ktluroberts',
-    count: 1
+    count: 1,
+    join_text: 'auto',
+    auto_join_text_reply: 'katie replied to',
+    auto_join_text_default: 'katie proclaimed,' 
   });
 
   $('div#bobby-tweet').tweet({
     username: 'bobbywilson0',
-    count: 1
+    count: 1,
+    join_text: 'auto',
+    auto_join_text_reply: 'bobby replied to',
+    auto_join_text_default: 'bobby proclaimed,'
   });
 
  /* $(".tweet").tweet({
